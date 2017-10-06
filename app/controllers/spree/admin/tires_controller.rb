c7fb2513b916008a1c8b46e4109bb7bcee3bce9c
Jorge Cardenas <jorge.cardenas@talentocorporativo.com>
2017-10-06 11:36:37 +0200

    Deleted scaffolding tests

--- app/controllers/spree/admin/tires_controller.rb
+++
@@ -1,9 +0,0 @@
-module Spree
-  module Admin
-    class TiresController < ResourceController
-      def index
-        @tires = Spree::Tire.page(params[:page] || 1).per(50)
-      end
-    end
-  end
-end
--- db/migrate/20171004120005_create_spree_marcas.rb
+++
@@ -1,8 +0,0 @@
-class CreateSpreeMarcas < ActiveRecord::Migration[5.1]
-  def change
-    create_table :spree_marcas do |t|
-      t.string :name
-      t.timestamps
-    end
-  end
-end
