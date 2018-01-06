c8d3f3b4de6d319d6cb2a5e281280d0e778ca00b
Jorge Cardenas <jorge.cardenas@talentocorporativo.com>
2018-01-06 22:53:11 +0100

    fixed some problemes

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,12 +1,10 @@
 class ContactsController < ApplicationController
   def contact
-    byebug
     if ContactMailer.contact(params[:contact]).deliver
       flash[:notice] = t('contact.notice.success')
     else
       flash[:error] = t('contact.notice.error')
     end
-byebug
     render :contact, format: :js
   end
 end
