41ce13c25951f854983a31e9687c9a5ee5f8a585
Jorge Cardenas <jorge.cardenas@talentocorporativo.com>
2018-03-07 08:35:14 +0100

    identation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,7 +12,8 @@ module ApplicationHelper
         phrase = Tolk::Phrase.where(key: args.first).first_or_create
         # if we found more args we check for default phrase translation
 
-        tlt = Tolk::Translation.where(phrase_id: phrase.id, locale_id: Tolk::Locale.find_by(name: I18n.locale).id).first_or_create
+        tlt = Tolk::Translation.where(phrase_id: phrase.id,
+                                     locale_id: Tolk::Locale.find_by(name: I18n.locale).id).first_or_create
         if tlt.new_record?
           if args.second.blank?
             tlt.text = ''
